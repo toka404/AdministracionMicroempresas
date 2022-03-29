57e2cbfd5182cdfa6f96cdd51470ad1c2dc4f1b9
alang <alang@DESKTOP-LLUH94C>
2022-03-28 19:13:03 -0500

    Add project files.

---
+++ BD.Web/Data/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BD.Web.Data
+{
+    public class MyDbContext: DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<provincia> provincia { get; set; }
+    }
+}
